b45e35597a6590cc956ee43aa8872de5fe01aad2
ameer s. hassoon <ameersafaa567@gmail.com>
2025-10-26 17:39:31 +0300

    added the MainBar.jsx component

--- src/components/MainBar.jsx
+++ src/components/MainBar.jsx
@@ -1,40 +1,38 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import logo from "../assets/ChatGPT Image Oct 26, 2025, 05_12_47 PM.png";
 
 export const MainBar = () => {
   const { currentUser, isAuthenticated } = useAuth();
 
   return (
-    <div className="relative">
-      <div className="bg-blue-950 w-full h-16">
-        <Link to="/">
-          <button className="text-3xl font-bold text-white absolute top-4 left-5 hover:cursor-pointer hover:text-violet-200 ">
-            movie it
-          </button>
-        </Link>
+    <div className="bg-blue-950 w-full h-16 flex items-center justify-between px-8">
+      <Link to="/" className="flex items-center">
+        <img src={logo} alt="logo" className="h-27 w-auto mt-5 " />
+      </Link>
 
-        <div className="absolute top-4 right-5 flex items-center gap-4">
-          {isAuthenticated ? (
-            <>
-              <span className="text-white text-lg">
-                Welcome, {currentUser?.name}
-              </span>
-              <Link to="/profile">
-                <img
-                  src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
-                  alt="Profile"
-                  className="w-10 h-10 rounded-full border-2 border-white hover:cursor-pointer"
-                />
-              </Link>
-            </>
-          ) : (
-            <Link to="/login">
-              <button className="text-xl text-gray-300 hover:text-white hover:bg-blue-900 px-3 py-1 rounded transition-colors">
-                Login
-              </button>
+      {/* Right side */}
+      <div className="flex items-center gap-5">
+        {isAuthenticated ? (
+          <>
+            <span className="text-white text-lg">
+              Welcome, {currentUser?.name}
+            </span>
+            <Link to="/profile">
+              <img
+                src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
+                alt="Profile"
+                className="w-10 h-10 rounded-full border-2 border-white hover:cursor-pointer"
+              />
             </Link>
-          )}
-        </div>
+          </>
+        ) : (
+          <Link to="/login">
+            <button className="text-xl text-gray-300 hover:text-white hover:bg-blue-900 px-3 py-1 rounded transition-colors">
+              Login
+            </button>
+          </Link>
+        )}
       </div>
     </div>
   );
